8692a8427bbf13282053c93cb8023ea6de87b1a6
dima burakov <nudnov2017@yandex.ru>
2023-11-01 14:37:55 +0300

    add011123

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import reportWebVitals from './reportWebVitals';
 import {store} from './redux';
 import {Provider} from 'react-redux';
 import {HashRouter} from 'react-router-dom';
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <HashRouter>
             <Provider store={store}>
-                <App/>
+                <DevSupport ComponentPreviews={ComponentPreviews}
+                            useInitialHook={useInitial}
+                >
+                    <App/>
+                </DevSupport>
             </Provider>
         </HashRouter>
     </React.StrictMode>
